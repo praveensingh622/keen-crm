21389a4eec9df19c812db26f6e261a5af3629931
synosure91 <pradeep.k@digitalobsession.in>
2025-01-17 22:17:32 +0530

    client directory add

---
+++ client/config.php
@@ -0,0 +1,11 @@
+<?php
+$username = "uziw4ais8741z";
+$password = '@e21tgf+$Dll';
+$host = "localhost";
+$database = "keen_crm";
+
+$conn = mysqli_connect($host, $username, $password, $database);
+if($conn){
+    echo "Data Connect";
+}
+?>
