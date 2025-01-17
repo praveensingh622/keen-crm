f5e7c7f838b764283193766c5252b3c173f05fd8
synosure91 <pradeep.k@digitalobsession.in>
2025-01-17 22:14:03 +0530

    config file add

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $username = "uziw4ais8741z";
 $password = '@e21tgf+$Dll';
 $host = "localhost";
-$database = "dbetgylgkg8sbd";
+$database = "keen_crm";
 
 $conn = mysqli_connect($host, $username, $password, $database);
 if($conn){
